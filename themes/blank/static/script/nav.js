3c1e304414ec4e30f60644adf99ecf912e08cb58
Cole R Lawrence <msgzht@gmail.com>
2017-10-05 21:31:21 -0500

    Trying crown nav link

--- themes/blank/static/script/nav.js
+++ themes/blank/static/script/nav.js
@@ -1,4 +1,4 @@
-;(function (document) {
+;(function (document, window) {
   function setupNavTransitions() {
     // move components to body
     let comps = Array.from(document.getElementById("nav-components").children)
@@ -13,7 +13,21 @@
     const navOverlay = document.getElementById("nav-overlay")
     navToggle.addEventListener("click", toggleNav)
     navOverlay.addEventListener("click", toggleNav)
-    document.getElementsByTagName("main")[0].appendChild(navToggle)
+    document.body.appendChild(navToggle)
+
+    // scrolling down detection
+    let previous = 0
+    let test = 0
+    const samples = 1
+    const recip = 1 / samples
+    const recipInf = (samples - 1) * recip
+    const navScrollingDown = 'nav-scrolling-down'
+    document.getElementById("page").addEventListener("scroll", function () {
+      test = previous * recipInf + this.scrollTop * recip
+      if (previous > test) { body.classList.remove(navScrollingDown) }
+      else { body.classList.add(navScrollingDown) }
+      previous = test
+    })
   }
   window.addEventListener("load", _ => setupNavTransitions(document))
-})(document);
+})(document, window);
