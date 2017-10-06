494d78d020191abcc7708511b952cdaa22d772cd
Cole R Lawrence <msgzht@gmail.com>
2017-10-05 20:43:02 -0500

    Write something

---
+++ content/pages/index.md
@@ -0,0 +1,10 @@
+---
+title: "index"
+draft: false
+type: "index"
+---
+# Howdy!
+
+I'm Cole, a software engineer & designer from Kansas City, Missouri.
+
+You can learn a bit more about me by reading [my posts](/posts) or by looking at [my resume](/2017/10/Cole_Lawrence_resume.pdf)
--- themes/blank/static/script/nav.js
+++ themes/blank/static/script/nav.js
@@ -1,5 +1,9 @@
 ;(function (document) {
   function setupNavTransitions() {
+    // move components to body
+    let comps = Array.from(document.getElementById("nav-components").children)
+    comps.forEach(elc => document.body.appendChild(elc))
+
     const body = document.body
     const navOpenClass = 'nav-open'
     function toggleNav() {
@@ -9,10 +13,7 @@
     const navOverlay = document.getElementById("nav-overlay")
     navToggle.addEventListener("click", toggleNav)
     navOverlay.addEventListener("click", toggleNav)
-
-    // move components to body
-    let comps = Array.from(document.getElementById("nav-components").children)
-    comps.forEach(elc => document.body.appendChild(elc))
+    document.getElementsByTagName("main")[0].appendChild(navToggle)
   }
   window.addEventListener("load", _ => setupNavTransitions(document))
 })(document);
